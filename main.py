0cb96f2348aab756f6b994bfff45569c11044587
asubhangi <subhangiagarwala@gmail.com>
2017-12-01 02:12:46 -0500

    thumbnailProcessing

--- main.py
+++ main.py
@@ -11,7 +11,11 @@ from authy.api import AuthyApiClient
 from pymongo import MongoClient
 from bson.objectid import ObjectId
 import datetime
-#from mongoengine import *
+from PIL import Image
+import base64
+from cStringIO import StringIO
+import simplejson as json
+import io
 
 # This defines a Flask application
 app = Flask(__name__)
@@ -74,7 +78,7 @@ def add_new_user():
     if body.get('first_name') is None:
         raise BadRequest('missing first name')
     if body.get('last_name') is None:
-        raise BadRequest('missing first name')
+        raise BadRequest('missing last name')
     if body.get('zipcode') is None:
         raise BadRequest('missing zip code')
     else:
@@ -97,6 +101,11 @@ def add_new_user():
     if body.get("vehicle") is None:
         raise BadRequest("Missing vehicle details")
 
+    if body.get("photo") is None:
+        raise BadRequest("Missing photo")
+    else:
+        data = imageStorage(body.get("photo"))
+
     password_hash = security.generate_password_hash(body.get('password'))
 
     newUser = {"firstName": body.get("first_name"),
@@ -107,6 +116,7 @@ def add_new_user():
                 "payment":body.get("payment"),
                 "phone": phone,
                 "vehicle": body.get("vehicle"),
+                "photo": data,
                 "verified_phone": False}    
 
     try:
@@ -116,6 +126,18 @@ def add_new_user():
 
     # check that mongo didn't fail
     return Response(status=201)
+
+def imageStorage(photo):
+    image = Image.open(photo)
+    findExtIndex = photo[::-1].find('.')
+    ext = photo[::-1][:findExtIndex][::-1]
+    #manipulate image size according to the requirement
+    image.thumbnail((100, 100), Image.ANTIALIAS)
+    output = StringIO()
+    image.save(output, format=ext)
+    im_data = output.getvalue()
+    data = base64.b64encode(im_data)
+    return data
 
 
 @app.route('/profile', methods = ['PUT'])
@@ -159,6 +181,13 @@ def update_profile():
         raise Unauthorized()
 
     return jsonify(session.get('user'))
+
+#the data passed should come from database - check the earlier implementation of the API
+def retriveImage(data):
+    decodedData = base64.b64decode(data)
+    buf = io.BytesIO(decodedData)
+    img = Image.open(buf)
+    return img
 
 
 @app.route('/verify', methods = ['POST'])
